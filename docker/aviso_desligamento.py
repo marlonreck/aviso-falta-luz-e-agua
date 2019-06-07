fb3c91db0155618ba8e365445d24d045db2cf413
marlonreck <38760760+marlonreck@users.noreply.github.com>
2019-06-07 10:54:36 -0300

    Create aviso_desligamento.py

---
+++ docker/aviso_desligamento.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 29 12:38:02 2019
+
+@author: marlon
+"""
+
+# python3-simplejson python3-pytelegrambotapi
+import requests
+from bs4 import BeautifulSoup
+import sys
+import telebot
+
+
+bot = telebot.TeleBot("734308366:AAGW6Pw5PwHNjj0LIvtGDkGedPhDuFNSxA8")
+# grupo aviso
+idgrupo = "-325650074"
+# grupo teste
+# idgrupo = "-350862650"
+
+
+def celesc(url, municipio):
+    pesquisa = {"munic": municipio}
+    try:
+        resposta = requests.post(url, data=pesquisa)
+    except Exception as erro:
+        return erro
+        sys.exit(1)
+    if resposta.status_code != 200:
+        return ("Site com erro: ", resposta.status_code)
+    else:
+        soup = BeautifulSoup(resposta.text, "html.parser")
+        tag = (soup.find('pre')).get_text()
+        tag = (tag.split('Bairro :'))
+        return tag
+
+
+def casan(url, municipio):
+    i = 0
+    lista = []
+    try:
+        resposta = requests.get(url)
+    except Exception as erro:
+        print(erro)
+        sys.exit(1)
+    if resposta.status_code != 200:
+        return ("Site com erro: ", resposta.status_code)
+    else:
+        soup = BeautifulSoup(resposta.text, "html.parser")
+        while i < len(soup.find_all("item")):
+            texto = str(soup.find_all("item")[i].get_text())
+            if texto.find("- "+municipio) != -1:
+                lista.append(soup.find_all("item")[i].get_text())
+            i += 1
+    return lista
+
+
+celesc_retorno = celesc("http://site.celesc.com.br/aplicativos/aviso_desligamento/index.php", "SAO JOSE")
+for iretorno in celesc_retorno:
+    bot.send_message(idgrupo, "CELESC: "+iretorno)
+
+casan_retornoSJ = casan("https://e.casan.com.br/avisos/rssfeed", "SÃO JOSÉ")
+if len(casan_retornoSJ) > 0:
+    bot.send_message(idgrupo, casan_retornoSJ)
+
+casan_retorno = casan("https://e.casan.com.br/avisos/rssfeed", "FORQUILHINHA")
+if len(casan_retorno) > 0:
+    bot.send_message(idgrupo, casan_retorno)
