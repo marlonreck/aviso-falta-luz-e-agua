ba62a2705b39a12b40ba22253f94dac926e29323
marlonreck <38760760+marlonreck@users.noreply.github.com>
2022-03-05 09:43:15 -0300

    Add files via upload

--- docker/aviso_desligamento.py
+++ docker/aviso_desligamento.py
@@ -3,7 +3,16 @@
 """
 Created on Wed May 29 12:38:02 2019
 
-@author: marlon
+@author: Marlon V. Reck
+e-mail: marlonreck@gmail.com
+Linkedin: https://br.linkedin.com/in/marlonreck
+
+Versão:1
+Desdescription:
+    Coleta avisos de falta de luz na celesc e falta de água na casan e envia 
+    para o grupo do telegram
+
+Requires: simplejson pyTelegramBotAPI beautifulsoup4
 """
 
 # python3-simplejson python3-pytelegrambotapi python3-ujson
@@ -11,13 +20,8 @@ import requests
 from bs4 import BeautifulSoup
 import telebot
 
-
-bot = telebot.TeleBot("734308366:AAGW6Pw5PwHNjj0LIvtGDkGedPhDuFNSxA8")
-# grupo aviso
-idgrupo = "-325650074"
-# grupo teste
-# idgrupo = "-350862650"
-
+bot = telebot.TeleBot("INCLUIR A CHAVE DO BOT AQUI")
+idgrupo = "INCLUIR O ID DO GRUPO DO TELEGRAM AQUI"
 
 def celesc(url, municipio):
     pesquisa = {"munic": municipio}
@@ -32,7 +36,6 @@ def celesc(url, municipio):
         tag = (soup.find('pre')).get_text()
         tag = (tag.split('Bairro :'))
         return tag
-
 
 def casan(url, municipio):
     i = 0
@@ -52,8 +55,6 @@ def casan(url, municipio):
             i += 1
     return lista
 
-
-# celesc_retorno = celesc("http://site.celesc.com.br/aplicativos/aviso_desligamento/index.php", "SAO JOSE")
 celesc_retorno = celesc("https://avisodesligamento.celesc.com.br/index.php", "SAO JOSE")
 for iretorno in celesc_retorno:
     bot.send_message(idgrupo, "CELESC: "+iretorno)
@@ -62,8 +63,3 @@ casan_retornoSJ = casan("https://e.casan.com.br/avisos/rssfeed", "SÃO JOSÉ")
 if len(casan_retornoSJ) > 0:
     for isjretorno in casan_retornoSJ:
         bot.send_message(idgrupo, isjretorno)
-
-casan_retorno = casan("https://e.casan.com.br/avisos/rssfeed", "FORQUILHINHA")
-if len(casan_retorno) > 0:
-    for iforquilhasretorno in casan_retorno:
-        bot.send_message(idgrupo, iforquilhasretorno)
