76b269cd95c7f7ad64858b69ccb612ffa9c9715f
marlonreck <38760760+marlonreck@users.noreply.github.com>
2025-02-22 22:58:23 -0300

    Atualizar o aviso_desligamento.py

--- docker/aviso_desligamento.py
+++ docker/aviso_desligamento.py
@@ -9,9 +9,9 @@ Linkedin: https://br.linkedin.com/in/marlonreck
 
 Versão:1
 Desdescription:
-    Coleta avisos de falta de luz na celesc por município e bairro,
+    Coleta avisos de falta de luz na celesc por município e bairro.
     Coleta avisos de falta de água na casan por município e envia para um 
-    grupo do telegram
+    grupo do telegram.
 
 Requires: simplejson pyTelegramBotAPI beautifulsoup4
 """
